2534ed396868c58682a3be9f5d1f256e60130c8c
IlliaSmolin <55273908+IlliaSmolin@users.noreply.github.com>
2020-10-06 22:04:19 +0300

    added dropdown tests structure

---
+++ describes/dropdown/dropdown.field.describe.js
@@ -0,0 +1,7 @@
+const dropdownPage = require("../../pages/web/dropdown.web.page");
+
+module.exports = function () {
+  beforeEach(function () {
+    dropdownPage.openUrl();
+  });
+}
---
+++ tests/dropdown.test.js
@@ -0,0 +1,5 @@
+const dropdownField = require("../describes/dropdown/dropdown.field.describe");
+
+describe("'Dropdown' page", function () {
+  describe("Dropdown field", dropdownField);
+});
